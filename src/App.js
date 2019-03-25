345fcb565c2c04cc6d782997583bd2f2590befad
ocralo <e_lopez1996@hotmail.com>
2019-03-25 01:15:40 -0500

    login de cambio de pagina

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Loadable from "react-loadable";
+import {HashRouter, Route, Switch} from "react-router-dom";
+
+const loading = () => <div>Cargando...</div>;
+const Login = Loadable({
+  loader: () => import('./views/Login'),
+  loading
+});
 
 class App extends Component {
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/login" name="Login - IMUAO" component={Login}/>
+        </Switch>
+      </HashRouter>
     );
   }
 }
