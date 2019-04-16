f2100d78c8f75527bd6e8f9a745f9086c0367564
ocralo <e_lopez1996@hotmail.com>
2019-04-16 00:27:11 -0500

    se crea el constructor del metodo card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,12 +3,24 @@ import "./Card.css";
 import Chip from "../chip/Chip";
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      lastName: this.props.lastName,
+      code: this.props.code,
+      email: this.props.email,
+      phone: this.props.phone
+    };
+    this.title = React.createRef();
+  }
+
   render() {
     return (
       <div className="col-md-4 col-sm-10 col-12">
         <div className="border mt-4 rel-card-background-top shadow">
           <div className="d-flex flex-column align-items-center p-4 rel-title-card-profile">
-            <h3 className="rel-text-title-card">Rodrigo Escobar Lopez</h3>
+            <h3 className="rel-text-title-card">{this.state.name} {this.state.lastName}</h3>
             <div className="rel-imagen-profile-card-out">
               <img
                 src="./assets/img/rodrigo.jpg"
@@ -21,14 +33,13 @@ export default class Card extends Component {
             <div className="rel-profile-card-personal-data">
               <h5 className="rel-title-card-body">Datos personales</h5>
               <div className="rel-body-card-profile-data">
-                <i className="far fa-envelope" />{" "}
-                rodrigo.escobar_lop@uao.edu.co
+                <i className="far fa-envelope" /> {this.state.email}
               </div>
               <div className="rel-body-card-profile-data">
-                <i className="fas fa-phone" /> 3124556789
+                <i className="fas fa-phone" /> {this.state.phone}
               </div>
               <div className="rel-body-card-profile-data">
-                <i className="far fa-id-badge mr-2" /> 2104567
+                <i className="far fa-id-badge mr-2" /> {this.state.code}
               </div>
             </div>
             <div className="rel-profile-card-personal-data">
