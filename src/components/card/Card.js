29892bc448c1e4b30b931189f177142393af1162
ocralo <e_lopez1996@hotmail.com>
2019-04-16 00:32:35 -0500

    se agrega la variable skills al metodo

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -10,7 +10,8 @@ export default class Card extends Component {
       lastName: this.props.lastName,
       code: this.props.code,
       email: this.props.email,
-      phone: this.props.phone
+      phone: this.props.phone,
+      skills: this.props.skills
     };
     this.title = React.createRef();
   }
@@ -20,7 +21,9 @@ export default class Card extends Component {
       <div className="col-md-4 col-sm-10 col-12">
         <div className="border mt-4 rel-card-background-top shadow">
           <div className="d-flex flex-column align-items-center p-4 rel-title-card-profile">
-            <h3 className="rel-text-title-card">{this.state.name} {this.state.lastName}</h3>
+            <h3 className="rel-text-title-card">
+              {this.state.name} {this.state.lastName}
+            </h3>
             <div className="rel-imagen-profile-card-out">
               <img
                 src="./assets/img/rodrigo.jpg"
@@ -33,13 +36,16 @@ export default class Card extends Component {
             <div className="rel-profile-card-personal-data">
               <h5 className="rel-title-card-body">Datos personales</h5>
               <div className="rel-body-card-profile-data">
-                <i className="far fa-envelope" /> {this.state.email}
+                <i className="far fa-envelope mr-2" />
+                {this.state.email}
               </div>
               <div className="rel-body-card-profile-data">
-                <i className="fas fa-phone" /> {this.state.phone}
+                <i className="fas fa-phone mr-2" />
+                {this.state.phone}
               </div>
               <div className="rel-body-card-profile-data">
-                <i className="far fa-id-badge mr-2" /> {this.state.code}
+                <i className="far fa-id-badge mr-2" />
+                {this.state.code}
               </div>
             </div>
             <div className="rel-profile-card-personal-data">
