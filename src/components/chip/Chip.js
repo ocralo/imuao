0eee392307fc5ca77829f277d5c4495336d3559e
ocralo <e_lopez1996@hotmail.com>
2019-04-17 23:11:29 -0500

    se agregan los datos que llegan a firebase a las chips de las cards

--- src/components/chip/Chip.js
+++ src/components/chip/Chip.js
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import "./Chip.css";
 
 export default class Chip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameProyect: this.props.nameProyect
+    };
+    
+  }
   render() {
     return (
-      <div className="rel-chip d-flex align-items-center border rounded-pill m-1 shadow-sm">
+      <div className="rel-chip d-flex align-items-center border rounded-pill m-1 bg-warning shadow-sm">
         <img
           className="rel-chip-img"
           src="./assets/img/Rodrigo.jpg"
           alt=""
         />
-        Pitochar
+        <span>{this.state.nameProyect}</span>
       </div>
     );
   }
