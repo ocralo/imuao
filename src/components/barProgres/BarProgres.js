c7c56c3326093648d6dacb37087203f7a553a772
ocralo <e_lopez1996@hotmail.com>
2019-04-19 01:09:55 -0500

    cambios en la clase

--- src/components/barProgres/BarProgres.js
+++ src/components/barProgres/BarProgres.js
@@ -13,7 +13,7 @@ export default class Login extends Component {
       width: this.state.percentage + "%"
     };
     return (
-      <div className="border rounded-pill w-75 ml-2 rel-fill-barprogres">
+      <div className="border rounded-pill w-75 rel-fill-barprogres">
         <div
           className="rounded-pill bg-warning d-flex justify-content-center text-white rel-barProgres-text"
           style={divStyle}
